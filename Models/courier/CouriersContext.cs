58b6d0b87c137d1d752a9a1e42ac3137619dd087
abdessamad berahhou <62484786+abdessamadberahhou@users.noreply.github.com>
2022-06-11 13:15:17 +0000

    Update CouriersContext.cs

--- Models/courier/CouriersContext.cs
+++ Models/courier/CouriersContext.cs
@@ -24,7 +24,7 @@ namespace CourierApi.Models.courier
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=tcp:couriermanagerdbserver.database.windows.net,1433;Initial Catalog=couriermanager;User Id=abdessamad@couriermanagerdbserver;Password=Berahhou@2001");
+                optionsBuilder.UseSqlServer("your_string_connection");
             }
         }
 
